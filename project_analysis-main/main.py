91ef1a882ff04c1776cc4d50eef00cc7f1b08fa5
Chirag Gupta <chiragggupta007@gmail.com>
2026-04-05 20:21:45 +0530

    Update main.py

--- project_analysis-main/main.py
+++ project_analysis-main/main.py
@@ -392,8 +392,7 @@ st.markdown("""
 
 /* Animated Gradient Background */
 .stApp {
-    # background: linear-gradient(270deg, #0a192f, #112240, #1c3d5a, #0a192f);
-    background:rgba(12,132,21,0.8);
+    background: linear-gradient(270deg, #0a192f, #112240, #1c3d5a, #0a192f);
     background-size: 400% 400%;
     animation: gradientWave 12s ease infinite;
     color: #e6f1ff;
