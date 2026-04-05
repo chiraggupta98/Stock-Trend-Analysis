f2eadc33baabf04b5ecc638431df589ee98c8620
Chirag Gupta <chiragggupta007@gmail.com>
2026-04-05 16:17:20 +0530

    Fix formatting for Meet the Founders section

--- project_analysis-main/main.py
+++ project_analysis-main/main.py
@@ -1109,7 +1109,7 @@ div[data-baseweb="select"] > div {
         
         st.markdown("---")
         
-        st.markdown("#### 👥 Meet the Founders")
+       st.markdown("#### 👥 Meet the Founders")
        st.markdown("### 👨‍💼 Founder")
         
         col1, col2, col3 = st.columns([1,2,1])
