ff369aac617f05049a5c81e345e4d0d5020769d4
Chirag Gupta <chiragggupta007@gmail.com>
2026-04-05 13:58:10 +0530

    Stock Trend Analysis

--- project_analysis-main/main.py
+++ project_analysis-main/main.py
@@ -208,7 +208,7 @@ if "page" not in st.session_state:
 # MODERN LOGIN PAGE
 # -------------------------------------------------------------------
 def login_page():
-     set_bg_image("image3.jpg")
+    set_bg_image("image3.jpg")
     st.markdown("<h1 style='text-align:center;'>🔐 Welcome to Stock Trend Analysis WebApp</h1>", unsafe_allow_html=True)
     # st.markdown("<h4 style='text-align:center;color:red;'>Login To  Stock Trend Analysis WebApp</h4>", unsafe_allow_html=True)
 
