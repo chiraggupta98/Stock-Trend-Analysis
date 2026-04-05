5423ff5df3e8be4f2de97173f9435784b6b5db3e
Chirag Gupta <chiragggupta007@gmail.com>
2026-04-05 14:43:52 +0530

    Change background color in main.py

--- project_analysis-main/main.py
+++ project_analysis-main/main.py
@@ -34,7 +34,7 @@ def set_bg_image(image_file, overlay=True):
             left: 0;
             width: 100%;
             height: 100%;
-           background: rgba(144, 238, 144, 0.25);
+           background: rgba(13, 21, 31, 25);
             z-index: 0;
         }
         """
