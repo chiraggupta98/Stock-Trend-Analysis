d7fb4991a7706dbfa24acf28f347d2e9dfdf2f4a
Chirag Gupta <chiragggupta007@gmail.com>
2026-04-08 10:39:43 +0530

    Update main.py

--- project_analysis-main/main.py
+++ project_analysis-main/main.py
@@ -1213,7 +1213,7 @@ div[data-baseweb="select"] > div {
     # ===============================================================
     # 🚀 AI PREDICTIONS & SIGNALS 
     # ===============================================================
-        with tab6:
+    with tab6:
 
             left, center, right = st.columns([0.1,20,0.1])
 
